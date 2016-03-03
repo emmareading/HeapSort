45b03aa090d461abd8d8e39207724c3f3f4df482
emmareading <readinge@uvic.ca>
2016-03-02 19:28:54 -0800

    Added files via upload

---
+++ HeapSort.java
@@ -0,0 +1,133 @@
+/* HeapSort.java
+   CSC 225 - Fall 2015
+   Assignment 3 - Template for Heap Sort
+   
+   This template includes some testing code to help verify the implementation.
+   To interactively provide test inputs, run the program with
+	java HeapSort
+
+   To conveniently test the algorithm with a large input, create a 
+   text file containing space-separated integer values and run the program with
+	java HeapSort file.txt
+   where file.txt is replaced by the name of the text file.
+
+   M. Simpson & B. Bird - 11/16/2015
+*/
+
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.Arrays;
+import java.io.File;
+
+//Do not change the name of the HeapSort class
+public class HeapSort{
+	/* HeapSort(A)
+		Sort the array A using heap sort.
+		You may add additional functions (or classes) if needed, but the entire program must be
+		contained in this file. 
+
+		Do not change the function signature (name/parameters).
+	*/
+	
+
+	public static void HeapSort(int[] A){
+
+		/* ... Your Code Here ... */
+
+		for (int i = A.length; i > 0; i--) {
+			buildHeap(A, i, A.length);
+		}
+		//ascending order 
+		for (int i = A.length-1; i > 0; i--) {
+			int temp = A[0];
+			A[0] = A[i];
+			A[i] = temp;
+			buildHeap(A, 1, i);
+		}
+	} 
+
+	public static void buildHeap(int[] A, int index, int length) {
+		int left = index*2; // left child 
+		int right = (index*2) + 1; //right child
+		int max = index;
+
+		if (left <= length && A[left-1] > A[index-1]) {
+			max = left;
+		}
+		if (right <= length && A[right-1] > A[max-1]) {
+			max = right;
+		}
+		if (index != max) {
+			swap(A, index, max, length);
+		}
+	} //end 
+
+	public static void swap(int[] A, int index, int max, int length) {
+		int temp = A[index-1];
+		A[index-1] = A[max-1];
+		A[max-1] = temp;
+		buildHeap(A, max, length);
+	}
+		
+
+
+	/* main()
+	   Contains code to test the HeapSort function. Nothing in this function 
+	   will be marked. You are free to change the provided code to test your 
+	   implementation, but only the contents of the HeapSort() function above 
+	   will be considered during marking.
+	*/
+	public static void main(String[] args){
+		Scanner s;
+		if (args.length > 0){
+			try{
+				s = new Scanner(new File(args[0]));
+			} catch(java.io.FileNotFoundException e){
+				System.out.printf("Unable to open %s\n",args[0]);
+				return;
+			}
+			System.out.printf("Reading input values from %s.\n",args[0]);
+		}else{
+			s = new Scanner(System.in);
+			System.out.printf("Enter a list of non-negative integers. Enter a negative value to end the list.\n");
+		}
+		Vector<Integer> inputVector = new Vector<Integer>();
+
+		int v;
+		while(s.hasNextInt() && (v = s.nextInt()) >= 0)
+			inputVector.add(v);
+
+		int[] array = new int[inputVector.size()];
+
+		for (int i = 0; i < array.length; i++)
+			array[i] = inputVector.get(i);
+
+		System.out.printf("Read %d values.\n",array.length);
+
+
+		long startTime = System.currentTimeMillis();
+
+		HeapSort(array);
+
+		long endTime = System.currentTimeMillis();
+
+		double totalTimeSeconds = (endTime-startTime)/1000.0;
+
+		//Don't print out the values if there are more than 100 of them
+		if (array.length <= 100){
+			System.out.println("Sorted values:");
+			for (int i = 0; i < array.length; i++)
+				System.out.printf("%d ",array[i]);
+			System.out.println();
+		}
+
+		//Check whether the sort was successful
+		boolean isSorted = true;
+		for (int i = 0; i < array.length-1; i++)
+			if (array[i] > array[i+1])
+				isSorted = false;
+
+		System.out.printf("Array %s sorted.\n",isSorted? "is":"is not");
+		System.out.printf("Total Time (seconds): %.2f\n",totalTimeSeconds);
+	}
+}
